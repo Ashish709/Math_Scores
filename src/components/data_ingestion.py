4a1e201b1e22f87d4c3b9cfc7fef5d2647011d04
Ashish709 <93144415+Ashish709@users.noreply.github.com>
2024-05-18 14:14:41 +0530

    data transformation and model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,22 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
-#python from logger import logging
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from pathlib import Path 
+sys.path.append(str(Path(__file__).parent.parent))
+from src.exception import CustomException
+# from exception import CustomException
+
+#from src.logger import logging
+from logger import logging
+
+
+from components.data_transformation import DataTransformation
+from components.data_transformation import DataTransformationConfig
+from components.model_trainer import ModelTrainingConfig,ModelTrainer
+
 
 # Defining the input for data Ingestion componet first
 @dataclass # it will help direct define class veriable with initializing (self)
@@ -51,11 +61,24 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion() # test data_ingestion.py only        
-            
-            
+# if __name__ == "__main__":
+#     obj = DataIngestion()
+    # obj.initiate_data_ingestion() # test data_ingestion.py only        
+    
+# to test Data transformation
+    # train_data,test_data = obj.initiate_data_ingestion()
+    # data_transformation = DataTransformation()
+    # data_transformation.initiate_data_transformation(train_data,test_data)    
 
+    
+        
 
+# to test model trainer
+    # train_data,test_data = obj.initiate_data_ingestion()
+    # data_transformation = DataTransformation()
+    # train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    # model_trainer = ModelTrainer()
+    # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+  
      
